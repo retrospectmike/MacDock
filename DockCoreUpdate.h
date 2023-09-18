a358f08416f332f11a6d71c907a790571043aaa9
Mike <69996986+retrospectmike@users.noreply.github.com>
2023-09-18 18:20:17 -0400

    dynamic width COMPLETE

--- DockCoreUpdate.h
+++ DockCoreUpdate.h
@@ -1,30 +1,1 @@
-#ifndef __COREUPDATE__
-#define __COREUPDATE__
-#include "QuickDraw.h"
-#include "ToolUtils.h"
-#include <GestaltEqu.h>
-#include <AppleEvents.h>
-#include "Processes.h" //for paramblockrec stuff and processLaunch
-
-
-#define kFinderICON_ID 129
-#define kGenAppICON_ID 130
-#define kDockIconWidth 32
-#define kDockIconHeight kDockIconWidth //square icons
-#define kDockHSpace 2
-#define kDockVSpace 2
-#define MAX_DOCK_ITEM_COUNT 8
-
-#define kBlinksOnClick 1 //number of blinks when an app is clicked
-#define kTicksBtwBlink 5 //60ths of a second between blinks
-
-
-
-void UpdateRunningApps(Boolean ForceRedraw);
-void OpenApp();
-Boolean LaunchApp(unsigned short);
-void FlashItem(unsigned short item_num);
-void InvertItem(unsigned short item_num);
-
-pascal Handle IconGetter(ResType iconType, Ptr data);
-#endif
+#ifndef __COREUPDATE__#define __COREUPDATE__#include "QuickDraw.h"#include "ToolUtils.h"#include <GestaltEqu.h>#include <AppleEvents.h>#include "Processes.h" //for paramblockrec stuff and processLaunch#define kFinderICON_ID 129#define kGenAppICON_ID 130#define kSmallIconHeight 16#define kLargeIconHeight 32#define kDockHBorderBig 6#define kDockHBorderSmall 3#define kDockHSpace 2#define kDockVSpace 2#define MAX_DOCK_ITEM_COUNT 8#define kBlinksOnClick 1 //number of blinks when an app is clicked#define kTicksBtwBlink 5 //60ths of a second between blinksvoid UpdateRunningApps(Boolean ForceRedraw);void OpenApp();Boolean LaunchApp(unsigned short);void FlashItem(unsigned short item_num);void InvertItem(unsigned short item_num);void UpdateDockSize(unsigned short NumApps);pascal Handle IconGetter(ResType iconType, Ptr data);#endif
